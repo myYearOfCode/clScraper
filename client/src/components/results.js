a9e2dc3598f4612e7d14238a3e1de2f5b03d56aa
Ross <Ross@myyearofcode.com>
2019-03-10 15:16:02 -0400

    working frontend => backend handoff.

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -5,10 +5,25 @@ class Results extends Component {
   constructor(props){
     super(props);
     this.state = {
-      recipeName: '',
-      data: []
+      data: [],
     }
-    // this.makePosts = this.makePosts.bind(this)
+  }
+
+  makePostDiv = (post) => {
+    return (
+      <div className = "outside" key={post.dataIdString}>
+      <div className = "photoDiv">
+      {this.getImage(post.dataIdString)}
+      </div>
+      <div className="text">
+      <a href = {post.link}>
+      <div className = {post.title} > {post.title} </div>
+      </a>
+      <div className = {post.price} > {post.price} </div>
+      <div className = {post.location} > Location: {post.location} </div>
+      <div className = "hideElement"> Hide </div>
+      </div>
+      </div>)
   }
 
   async componentDidMount() {
@@ -19,44 +34,54 @@ class Results extends Component {
       return response.json() //creates its own promise?!?!
     })
     .then(data => {
-      console.log(data);
       this.setState({data: data})
+      console.log(this.state.data);
+      return (Object.keys(this.state.data)[0])
+      if (Object.keys(this.state.data).length > 0) {
+        return Object.keys(this.state.data).map(key => {
+          let post = this.state.data[key]
+          return this.makePostDiv(post)
+        })
+      }
+      // this.setState({content: output})
     })
   }
 
+  getImage = (dataId,size) => {
+    // sizes: 50x50 300x300 600x450
+    let images = (this.parseDataId(dataId));
+    // return images.map((image) => {
+    let url = `https://images.craigslist.org/${images[0]}_300x300.jpg`
+    return (
+      <img src = {url} />
+    )
+  }
+
+  parseDataId = (dataId) => {
+    let imageArray = dataId.split(",")
+    imageArray = imageArray.map((imageId) => {
+      return (imageId.split(":")[1])
+    })
+    return imageArray
+  }
+
+
   makePosts = () => {
-    let output = ""
-    if (Object.keys(this.state.data) > 0) {
-      (Object.keys(this.state.data)).forEach(post => {
-        output += `
-          <div class = "outside">
-            <div class = "photoDiv">
-              ${this.getImage(post.dataIdString)}
-            </div>
-            <div class="text">
-              <a href = "${post.link}">
-                <div class = "${post.title}" > ${post.title} </div>
-              </a>
-              <div class = "${post.price}" > ${post.price} </div>
-              <div class = "${post.location}" > Location: ${post.location} </div>
-              <div class = "hideElement"> Hide </div>
-            </div>
-          </div>`
+    // how can I get this to wait for the api call before it processes?
+    // or at least refresh after the state is updated?
+    if (Object.keys(this.state.data).length > 0) {
+      return Object.keys(this.state.data).map(key => {
+        let post = this.state.data[key]
+        return this.makePostDiv(post)
       })
     }
-    console.log(output)
-    // this.setState({content: output})
-    return output
   }
 
   render () {
     return (
-          // <div className = "wrapper">
-          <div>
-            {this.makePosts()}
-          </div>
-          // </div>
-          // <div>hi</div>
+      <div className="wrapper">
+        {this.makePosts()}
+      </div>
     )
   }
 }
